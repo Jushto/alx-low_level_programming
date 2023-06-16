28ed269e8cacaf996cd6a878fbbd879e14b6ae81
Justin <jg.riungu@gmail.com>
2023-06-15 19:56:20 -0700

    1-1- quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main -Entry point
+ * sends a custom error message to the standard output
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
