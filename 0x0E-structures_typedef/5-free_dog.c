8f4c6ae0d9f8a9a84230c8e3ae175e24f659e6de
Justin Riungu <jg.riungu@gmail.com>
2023-09-12 22:29:21 +0300

    struct4

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * free_dog - frees memory allocated for struct dog
+ * @d: pointer to memory block to be freed
+ *
+ * Return: nothing
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -2,4 +2,5 @@
 1 - initilize a variable of stype struct dog
 2 - print a struct dog
 3 - typedef dog_t as new name for struct dog
-
+4 - create a new dog
+5 - free dogs
