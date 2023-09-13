998e9a0a68808df71226cab3fed2e6df2c6561eb
Justin Riungu <jg.riungu@gmail.com>
2023-09-13 17:39:28 +0300

    funcpoint1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function given as a parameter for each element of array
+ * @array: pointer to array
+ * @size: size of array
+ * @action: pointer to needed function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+	
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -1,1 +1,2 @@
 0 - function to print a name, using a callback function to perform different operations on the name string
+1 - function executina function given as parameter
