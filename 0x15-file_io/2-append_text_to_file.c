73f375b84b789031d70d2d9199eb44034483429f
Justin Riungu <jg.riungu@gmail.com>
2023-10-03 11:57:14 +0300

    fileio2-correct

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * append_text_to_file - appends text to end of file
+ * @filename: name of file
+ * @text_content: NULL terminated string at end of file
+ *
+ * Return: 1 on success; -1 on failure
+ */
+
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, len, wr;
@@ -13,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		len = 0;
-		while(text_content[len] != '\0')
+		while (text_content[len] != '\0')
 			len++;
 		wr = write(fd, text_content, len);
 		if (wr == -1)
